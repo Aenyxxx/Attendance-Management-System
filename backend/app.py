b56b8505762d73b19c84e213b28bf30cde977582
NYX X <peraltajohnnic@gmail.com>
2026-09-16 22:33:35 +0800

    Added Module for backend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,55 @@
 from flask import Flask, jsonify
+import os
+import psycopg
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
+import os
+
+import psycopg
+from dotenv import load_dotenv
+
+load_dotenv()
+
+try:
+    connection = psycopg.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+    )
+
+    print("Database connection successful!")
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    SELECT
+        public.transaction.id,
+        public.transaction.description,
+        public.transaction.amount,
+        public.categories.name
+    FROM public.transaction
+    JOIN public.categories
+        ON public.transaction.category_id = public.categories.id
+    ORDER BY public.transaction.amount DESC
+    LIMIT 5;
+    """)
+    transactions = cursor.fetchall()
+
+    print("Tables in the public schema:")
+    for transac in transactions:
+        print(transac)
+
+    cursor.close()
+    connection.close()
+except Exception as error:
+    print("Database connection failed!")
+    print(error)
 
 @app.route("/")
 def home():
--- backend/db_test.py
+++ backend/db_test.py
@@ -1,43 +1,7 @@
-import os
+from db import get_db_connection
 
-import psycopg
-from dotenv import load_dotenv
+connection = get_db_connection()
 
-load_dotenv()
+print("Connected Successfuly!");
 
-try:
-    connection = psycopg.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-    )
-
-    print("Database connection successful!")
-
-    cursor = connection.cursor()
-
-    cursor.execute("""
-    SELECT
-        public.transaction.id,
-        public.transaction.description,
-        public.transaction.amount,
-        public.categories.name
-    FROM public.transaction
-    JOIN public.categories
-        ON public.transaction.category_id = public.categories.id
-    ORDER BY public.transaction.amount DESC
-    LIMIT 5;
-    """)
-    transactions = cursor.fetchall()
-
-    print("Tables in the public schema:")
-    for transac in transactions:
-        print(transac)
-
-    cursor.close()
-    connection.close()
-except Exception as error:
-    print("Database connection failed!")
-    print(error)
+connection.close();
